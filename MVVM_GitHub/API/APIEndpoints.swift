f73dfd9488b7bd5a2ac5b5ea9e34578b3cd6d861
bartlomiejbocian <bartlomiej.bocian@3e.pl>
2018-01-03 08:47:00 +0100

    first commit

---
+++ MVVM_GitHub/API/APIEndpoints.swift
@@ -0,0 +1,34 @@
+//
+//  APIEndpoints.swift
+//  MVVM_GitHub
+//
+//  Created by Bartłomiej Bocian on 02.01.2018.
+//  Copyright © 2018 Bartłomiej Bocian. All rights reserved.
+//
+
+import Foundation
+
+enum APIEndpoints {
+    case repositories(query: String)
+    case users(query: String)
+}
+
+extension APIEndpoints {
+    
+    var basePath: String {
+        return "https://api.github.com/search/"
+    }
+    
+    var baseParams: String {
+        return "&sort=id&order=asc"
+    }
+    
+    var path: URL {
+        switch self {
+        case .repositories(let query):
+            return URL(string: basePath + "repositories?q=" + query + baseParams)!
+        case .users(let query):
+            return URL(string: basePath + "users?q=" + query + baseParams)!
+        }
+    }
+}
---
+++ MVVM_GitHub/Model/Repo.swift
@@ -0,0 +1,9 @@
+//
+//  Repo.swift
+//  MVVM_GitHub
+//
+//  Created by Bartłomiej Bocian on 02.01.2018.
+//  Copyright © 2018 Bartłomiej Bocian. All rights reserved.
+//
+
+import Foundation
